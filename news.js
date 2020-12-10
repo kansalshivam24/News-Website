4e92fbff9b43a658bbb53b399e48a3812990e8a0
kansalshivam24 <44227500+kansalshivam24@users.noreply.github.com>
2020-12-10 22:43:19 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    
+    return getNews();
+});
+
+
+
+function getNews()
+{
+
+
+    $.ajax({
+        url: "https://api.currentsapi.services/v1/search?country=IN"+"&apiKey=tP0paEvRQhiz6S7gpQ7JRILx3mp6KwWZ332l9K53m5O9e0zw",
+        type: "GET",
+        dataType: "json",
+        success: function(data){
+
+            var table="";
+
+            for(var i=0;i<data.news.length;i++)
+            {
+                table=table+'<div class="card mb-12" key='+i+'>';
+                table=table+'<div class="row"><div class="col-mb-12"style="background-color:#bf80ff">';
+                table=table+ '<div class="card-body" > <marquee><h5 class="card-title" style="text-shadow: 2px 2px #ff0000;font-size:40px;">'+data.news[i].title+'</h5></marquee>';
+                table=table+ '<p class="card-text" style="font-weight:bold">'+data.news[i].description+' </p>';
+                table=table+ '<p class="card-text"> <small class="text-muted" style="color:white"><p >'+data.news[i].author+'</p><p >'+data.news[i].published+'</p></small></p> </div> </div> </div>';
+               
+           }
+
+            $("#output-news").html(table);
+         
+        }
+    });
+
+}
+
+
+
